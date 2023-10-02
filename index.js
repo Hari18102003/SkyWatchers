53d52bf92946e2839c737ae4ab28e0620d0ec2aa
Hari <harishnathr@gmail.com>
2023-10-02 20:08:54 +0530

    another commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const url = "https://api.nasa.gov/planetary/apod";
 const api_key = "Ke84fc3H4eVlctR1xTKQErIaKsJNFpslVgBzWMtd";
 
--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
 const menu = $(".menu");
 const navItems = $(".nav-items");
-
 menu.click(function () {
     if (navItems.hasClass("active")) {
         navItems.removeClass("active");
@@ -9,3 +8,27 @@ menu.click(function () {
         navItems.addClass("active");
     }
 });
+
+function emailMe(){
+    const userEmail = $(".mail").val();
+    const feedBack = $(".textarea").val();
+    const body = "Email: "+userEmail+"<br>Feedback: "+feedBack;
+    console.log(userEmail+","+feedBack+","+body);
+    Email.send({
+        SecureToken : "ece92d04-c526-4e6a-9ad2-86d29a4f77dc",
+        To : 'harishnathr@gmail.com',
+        From : "harishnathr@gmail.com",
+        Subject : "A feedback",
+        Body : body
+    }).then(
+      message => {
+        if(message == 'OK'){
+            swal("Mail Sent Successfully", "Thankyou for your feedback", "success");
+        }
+      }
+    );
+    
+    
+
+    
+}
